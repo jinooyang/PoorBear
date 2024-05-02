8373f9de25c206c3167b1c7db720179bf54f75a9
jinooyang <jinooyang@gmail.com>
2024-05-02 15:36:45 +0900

    fix : camera
    Former-commit-id: 5eba811fb53211c5ec1577ba5fbbc3b7ddb59a7b

--- PoorBear/Assets/Scripts/BasicSpawner.cs
+++ PoorBear/Assets/Scripts/BasicSpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a34046583347df5cedd974fb4eb16153f6d6ebaadbf7819a6c934aa4c079d80
-size 5217
+oid sha256:0e3e2acc461b0bd3b05906a3d2d6424ed314e6e7572ca5b89a7a5dac726a84e7
+size 5222
