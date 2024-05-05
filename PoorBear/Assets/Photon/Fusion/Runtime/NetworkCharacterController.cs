1c8d33bc51d66647612c446e25f0b343b0c6356c
zqwz <zqwz@naver.com>
2024-05-05 21:50:16 +0900

    fix: camera, attack sync fix
    Former-commit-id: 752e735b228f2b7fd0300e2b554107865b37790f

--- PoorBear/Assets/Photon/Fusion/Runtime/NetworkCharacterController.cs
+++ PoorBear/Assets/Photon/Fusion/Runtime/NetworkCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2edd5e5ead404cde12604ecd4766bb5263747c69ca9ff4d75f8447dbd361bec3
-size 7579
+oid sha256:836dadc92895e7bb17089cfacf0c52641828d80d844b2892b9d49474b1b52c14
+size 8159
--- PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
+++ PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68fc3e4c4a2ddad631841bda2123cf52152b6827d78d669fecb93c987d469fa7
-size 19169
+oid sha256:44e908c851bea86c525c2910fc9efc30242395a5ba59601506cd6f83d7edd93f
+size 19137
