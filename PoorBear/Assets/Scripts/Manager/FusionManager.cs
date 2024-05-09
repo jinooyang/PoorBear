e85ab302e0c16532ddb8ba4c5af1874e69c8b682
yeonmi <wkdus885@naver.com>
2024-05-09 17:45:47 +0900

    feat : footstep
    Former-commit-id: e081d06875d8c823fe898b0a8c8668347eb72b72

--- PoorBear/Assets/Scripts/Manager/FusionManager.cs
+++ PoorBear/Assets/Scripts/Manager/FusionManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61e2965d9ff88879ae5cb78730e4b8c15a3509df3bd0efd74f1097d7f757e979
-size 5535
+oid sha256:928a507a9ec6b8cff63dd5e9d64d97bbd9ce103f14f1df4a7e0c57d735f30f50
+size 5671
---
+++ PoorBear/Assets/Scripts/SurviveMap/StepScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac552ca41e18ffe3e7e0ecb08f60ef4d73dc9e9f7c612020f7159ef36cf7f1f1
+size 2098
