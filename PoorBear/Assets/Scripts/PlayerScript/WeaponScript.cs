94b2a1abf65e2f886efdcb743ecd2bdcfda70296
zqwz <zqwz@naver.com>
2024-05-09 17:29:16 +0900

    test
    Former-commit-id: af5db906002dac28c127a7d6e402853cd8c474f9

--- PoorBear/Assets/Scripts/PlayerScript/WeaponScript.cs
+++ PoorBear/Assets/Scripts/PlayerScript/WeaponScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f883d973d87a80b7183b04ef7913ab88b1d5af952f80e1f4da124ee478ed0e9
-size 5844
+oid sha256:db44c2c576131c56c0a2dfe77a45520a5e6028b060792a7b251e440dd0336fa9
+size 6020
