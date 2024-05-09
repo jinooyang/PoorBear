04df6ec816dc18049eac73769da09b2038c7500c
박연서 <qkrdustj45@naver.com>
2024-05-09 10:52:45 +0900

    style: 로그 지움...
    Former-commit-id: a4231426fa8acfed33c09822bdc84262f1d5f672

--- PoorBear/Assets/Scripts/UI/UserBlockItem.cs
+++ PoorBear/Assets/Scripts/UI/UserBlockItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a32d9987884ca877a3e9cf9c7f849a1e10853d067f14b77076a6e2ef81406f9c
-size 1567
+oid sha256:8e04f803901832b70fe48daa99b781ee7a6d1991e86dc4baf03928f243686c74
+size 1511
