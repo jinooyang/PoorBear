b95d211139d97e84a830f5b6f8c2a6eaaa865b70
김세리 <onid057@gmail.com>
2024-05-09 18:01:19 +0900

    test
    Former-commit-id: abd4bbbbcceae2ddd86db94d141f5b6064397bb5

--- PoorBear/Assets/Scripts/AnimationSyncScript.cs
+++ PoorBear/Assets/Scripts/AnimationSyncScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:befd0424096bdbbf59b7b1ffe98a70d66d5f196162d058e191b19680a7777c38
-size 1130
+oid sha256:8df895c04203e1d13514173dc5317489f4a75735fad646dab00a75ec8cf2b052
+size 1132
