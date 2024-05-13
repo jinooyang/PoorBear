8ddd1dd01a40095f4b155f152b7bbf8ef7f502c5
박연서 <qkrdustj45@naver.com>
2024-05-13 10:36:41 +0900

    fix: tag누락
    Former-commit-id: 357d6361fa8699b0837f2cef53d065d127e3ea74

--- PoorBear/Assets/Scripts/Manager/ManagerContainer.cs
+++ PoorBear/Assets/Scripts/Manager/ManagerContainer.cs
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:12a7e57553a4751e9f4eae85ec7756712d2111e01548aee9d8f2c2871d7d2fad
-size 263
+using FusionExamples.Utility;
+using UnityEngine;
+
+public class ManagerContainer : MonoBehaviour
+{
+    
+    public static ManagerContainer Instance => Singleton<ManagerContainer>.Instance;
+
+    void Start()
+    {
+        // DontDestroyOnLoad(gameObject);
+    }
+
+}
