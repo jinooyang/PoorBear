c5b31416d47fd9f6878b73e3700bf287deff95ee
seongeun <gkapf838@gmail.com>
2024-05-14 12:45:44 +0900

    feat: 달리기 맵 수정 및 리스폰 적용
    Former-commit-id: 787dfd07c265d2d208a31d27a958524e0c469d7e

--- PoorBear/Assets/Scripts/Manager/PlayerManager.cs
+++ PoorBear/Assets/Scripts/Manager/PlayerManager.cs
@@ -1,15 +1,3 @@
-using System;
-
-
-public class PlayerManager
-{
-    private PlayerManager() { }
-
-    private static readonly Lazy<PlayerManager> Manager
-        = new Lazy<PlayerManager>(()=>new PlayerManager());
-    public static PlayerManager Instance => Manager.Value;
-    public string UserNickname { get; set; }
-    public string SessionNumber { get; set; }
-    public bool IsHost { get; set; }
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e43364005126fd2910aa86c812c7e757f8a0974d7b8da91bc373c5d77e754a0
+size 388
