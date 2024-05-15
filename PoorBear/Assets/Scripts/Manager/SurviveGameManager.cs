4c77ecbff238d1ca6060dc139c70388879ea6a3b
zqwz <zqwz@naver.com>
2024-05-15 20:20:07 +0900

    feat: 살아남기맵 게임 매니저 및 탈락 판정
    Former-commit-id: dbaa6a5686f448bea191ce85fa501027f069785f

---
+++ PoorBear/Assets/Scripts/Manager/SurviveGameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06ba78e4ee1dbd48b6ea0f31cbe41ece2c05af7520a190f4ee6a006763fd3d9e
+size 1939
---
+++ PoorBear/Assets/Scripts/SurviveMap/Death.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00fb483a3b5c77b5963beb8d055eccb4893bedcf47c8a68a55a7301bd9ca9037
+size 691
