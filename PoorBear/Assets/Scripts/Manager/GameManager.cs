f3847cfcd2cbec1bfc3c7467586fc9a2f0f095ab
seongeun <gkapf838@gmail.com>
2024-05-16 23:41:51 +0900

    fix: conflicts 해결
    Former-commit-id: a3944c687794346b7ec377194fa9b0703e4c5545

--- PoorBear/Assets/Scripts/Manager/GameManager.cs
+++ PoorBear/Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a807e536823b6e4bfea5b49a7b37f08c734718b8dabe2a705cfe56a5b5776c7
-size 1771
+oid sha256:44e844da86cde8e6832fd59667798aa28be5668028e825d92109043fdc6a69f7
+size 1691
--- PoorBear/Assets/Scripts/Manager/LevelManager.cs
+++ PoorBear/Assets/Scripts/Manager/LevelManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92311e88846fb80d6ae778f06cdb40a2037b9bffb221a7a2a0e75a33a23f79e3
-size 765
+oid sha256:a73f1080b25a55632a4c93dea0d8646f48e5b2b11c7ad104e3d77857883bf1cf
+size 1216
