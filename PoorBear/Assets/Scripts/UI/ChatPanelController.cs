eea84f2f9561f7a7cdd6f9e746f44d0a7aa98fb7
박연서 <qkrdustj45@naver.com>
2024-05-17 20:41:41 +0900

    fix: 채팅로그 진짜진짜진짜마지막
    Former-commit-id: 6976d440d33f0c18d32637c52c1d0aa5876aece9

--- PoorBear/Assets/Scripts/UI/ChatPanelController.cs
+++ PoorBear/Assets/Scripts/UI/ChatPanelController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfe0217e0ceb028c91ad0fde88e716fb13b1bbb8c5e9f175541f99367f695757
-size 1680
+oid sha256:eeda10071adc9648af623e2a093ca70ad739de6e542fa5a4c5321d6007cfc82b
+size 1870
