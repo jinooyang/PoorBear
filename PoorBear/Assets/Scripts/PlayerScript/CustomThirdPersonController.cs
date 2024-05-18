56fed60f8007dddee4e400d0c9c68b3c93735827
yeonmi <wkdus885@naver.com>
2024-05-18 15:23:29 +0900

    merge start
    Former-commit-id: 6b598804d5670e899bf143de12233aa92465806d

--- PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
+++ PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98a24b521a3d4ba8fa3cd4d679e925ba5f854d5d192a7588c5f2f58655f3bdf
-size 28969
+oid sha256:8a183da946c4971945240f6d8f5a5e974c253cc1d9a4a853de5c31fd799e58b2
+size 29005
