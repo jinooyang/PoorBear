71a943b5ca532cee803d2d540d2d6260fa6b6133
박연서 <qkrdustj45@naver.com>
2024-05-18 21:55:37 +0900

    fix: password room
    Former-commit-id: 53262093cadfbaa58fd2cf28d48abfede2b633e2

--- PoorBear/Assets/Scripts/UI/PasswordInputPopupUI.cs
+++ PoorBear/Assets/Scripts/UI/PasswordInputPopupUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35408301a98648a7bb01b2416a1cdc2f7ee8435acfa9bc74b8c8ccb841c5ba62
-size 1529
+oid sha256:99b31b2a7275bfbb2dee55f3d20a99e4e943ede92f85e206b3eae8d0c13d98fc
+size 1643
--- PoorBear/Assets/Scripts/UI/SessionRoom.cs
+++ PoorBear/Assets/Scripts/UI/SessionRoom.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c51de6b07613fc04133beb21339580707af18a5c07125f362c9e18d237540fe
-size 2699
+oid sha256:f6bcc18c1cc8376f516f8553b4c499556cf7924a4bf6c26111a446f6a5112a1d
+size 2852
