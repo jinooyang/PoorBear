a6bade706879770b674112418cbb9860d17b0745
zqwz <zqwz@naver.com>
2024-05-19 00:21:21 +0900

    fix: 게임 모드 이름 변경
    Former-commit-id: a2954c81e652d47d411c13c99778f5b5e29b23d9

--- PoorBear/Assets/Scripts/Network/ServerInfo.cs
+++ PoorBear/Assets/Scripts/Network/ServerInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5af1a100aeb69148cdf38589a7e38be1e89a0dfe98d0abc50f1ed2d0f58a33f5
-size 957
+oid sha256:2d443b71e4a73cf53bc3025584f14c080ce5ff616f54af92b7ec058f2807169c
+size 954
