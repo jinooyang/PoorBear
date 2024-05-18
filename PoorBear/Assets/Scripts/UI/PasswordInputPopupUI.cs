fe6f89331d1795b23ba3f1c252da0a4f692b7ebb
박연서 <qkrdustj45@naver.com>
2024-05-18 18:44:15 +0900

    Merge specific files MainScene, roomPrefab, PasswordInputPopupUI
    Former-commit-id: c9dcaaed16a90bfe070c5e358b3fde10dee779ff

--- PoorBear/Assets/Scripts/UI/PasswordInputPopupUI.cs
+++ PoorBear/Assets/Scripts/UI/PasswordInputPopupUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b470d8d7955dc0c25cdec6dca590616af75338ca893832a049a0a011337a8ec
-size 1029
+oid sha256:35408301a98648a7bb01b2416a1cdc2f7ee8435acfa9bc74b8c8ccb841c5ba62
+size 1529
