dd1f7e77b64c240e0ad503253f41fb1c6e416963
yeonmi <wkdus885@naver.com>
2024-05-18 20:26:47 +0900

    feat : 뚝배기 WEAPON
    Former-commit-id: 20a13615966283ecb031fa15da014a288cd901f6

--- PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
+++ PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb5e124641783ce29e79ffce088eb515afd2bc6f3ee107c00ee08616cd53b428
-size 24095
+oid sha256:3a70faaffa267d767cbc9581217666a07efa1698a5961961267a8d2e1b11a650
+size 24119
