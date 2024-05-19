acc75c3755c1a03a26bb99c7fb43b7420c0f187e
seongeun <gkapf838@gmail.com>
2024-05-20 08:58:26 +0900

    fix: R 리스폰 삭제
    Former-commit-id: 658409ee412cc14c71d636155b9586fc7a8bbc99

--- PoorBear/Assets/Scripts/Network/GameLuncher.cs
+++ PoorBear/Assets/Scripts/Network/GameLuncher.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60703d75d57d33dce9c7ef2daec3e8cc1c96edcbe32255d5460ec99fa340975f
-size 10156
+oid sha256:8383c9d6afc0e7a318e7710c1ae4f795a421d89a155c0e4220395a510f0bc75b
+size 9863
