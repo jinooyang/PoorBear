e06d4c514b10c1ed645aa2c60372f584b6bfd0a1
zqwz <zqwz@naver.com>
2024-05-19 20:14:51 +0900

    fix: 게임 시작시 커서 잠금 추가
    Former-commit-id: 6d8d6c5e72ff2bfef56afdd2fa6019ea057d3145

--- PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
+++ PoorBear/Assets/Scripts/PlayerScript/CustomThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5072ca88e85f507788a94448cd9c4ecf43b82e817bc5c53bd1082f79130cdea
-size 24297
+oid sha256:9cd6028a649dc53a40cb662b0c5a51b2da1f03478ced8911fa60137d74d6ba5b
+size 24351
